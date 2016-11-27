7f72326e8ee8946c2ef02838dbdb473b33ed3add
Svyatoslav <barsic.plus@gmail.com>
2016-11-27 21:15:46 +0200

    New improvements and bugs fixed

--- js/script.js
+++ js/script.js
@@ -1,18 +1,24 @@
 var app = angular.module("workedCalc", []);
 app.controller("calcCtrl", function($scope) { 
   $scope.records = [[0, 0]];
-   // localStorage.setItem("records", $scope.records);
 
+  // Get rate from localStorage
   if(localStorage.getItem("rate") != null)
     $scope.rate = parseInt(localStorage.getItem("rate"));
   else
     $scope.rate = 0;
 
+  // Get table records from localStorage
   if(localStorage.getItem("records") != null)
     $scope.records = JSON.parse(localStorage.getItem("records"));
   else
     $scope.records = [[0, 0]];
 
+
+  /**
+   * Remove elect row
+   * @param index index
+   */
   $scope.removeCol = function(index) {
     $scope.records.splice(index, 1);
 
@@ -20,19 +26,91 @@ app.controller("calcCtrl", function($scope) {
       $scope.records = [[0, 0]];
   }
 
+
+  /**
+   * Setup file data after button clicked
+   */
+  $scope.saveToFile = function() {
+    var time_string = '';
+    for(var i = 0; i < $scope.records.length; i++) {
+      time_string += i+1 + '. ' + $scope.records[i][0] + 'h ' + $scope.records[i][1] + 'm\r\n'
+    }
+
+    var data = getClearDate() + '           Worked Time Calculator\r\n' +
+      '----------------------------------------------------\r\n \r\n' +
+      time_string + '\r\n' +
+      'Total Time: ' + $scope.getTime().h + 'h ' + $scope.getTime().m + 'm\r\n' +
+      'Money Earned: $' + $scope.getMoney();
+    var fileName = "Worked Time.txt";
+    saveData(data, fileName);
+  }
+
+
+  /**
+   * Return data in clear string format
+   * @return string
+   */
+  function getClearDate() {
+    var date = new Date();
+
+    var y = date.getFullYear();
+    var m = date.getMonth()+1;
+    var d = date.getDate();
+
+    var hr = date.getHours();
+    var mr = date.getMinutes();
+    var sr = date.getSeconds();
+
+    return(d+'.'+m+'.'+y+' '+hr+':'+mr+':'+sr);
+  }
+
+
+  /**
+   * Save data to .txt file
+   * @param  string data
+   * @param  string fileName
+   */
+  function saveData(data, fileName) {
+    var a = document.createElement("a");
+    document.body.appendChild(a);
+    a.style = "display: none";
+    blob = new Blob([data], {type: "octet/stream"}),
+    url = window.URL.createObjectURL(blob);
+    a.href = url;
+    a.download = fileName;
+    a.click();
+    window.URL.revokeObjectURL(url);
+  }
+
+
+  /**
+   * Save rate to local Storage
+   */
   $scope.changeRate = function() {
     localStorage.setItem("rate", $scope.rate);
   }
 
+
+  /**
+   * Save input's data to local Storage
+   */
   $scope.changeValues = function() {
     localStorage.setItem("records", JSON.stringify($scope.records));
   }
 
+
+  /**
+   * Add new table rows
+   */
   $scope.addCol = function() {
     $scope.records.push([0, 0]);
     $scope.changeValues();
   }
 
+
+  /**
+   * Delete all table rows
+   */
   $scope.clearAll = function() {
     if($scope.records.length > 1) {
       var conf = confirm('Are you sure?');
@@ -43,6 +121,11 @@ app.controller("calcCtrl", function($scope) {
     }
   }
 
+
+  /**
+   * Count and return total time
+   * @return object Get total hours and minutes
+   */
   $scope.getTime = function() {
     var h = 0, m = 0;
 
@@ -61,6 +144,11 @@ app.controller("calcCtrl", function($scope) {
     return { h: h, m: m }
   }
 
+
+  /**
+   * Count and return total earned money
+   * @return float Get total money based on time and rate
+   */
   $scope.getMoney = function() {
     var time = $scope.getTime();
 
